6fb4e4590ef53590f8203a159a00d567257f5bcb
Na-tallia <mlz.kng@gmail.com>
2026-02-26 21:54:13 +0300

    Fixet project structure and added frontend files

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="p-20">
+      <h1 className="text-4xl font-bold">Привет, это мой фронтенд! 👋</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        Сейчас я научусь подключать сюда Python.
+      </p>
+    </div>
+  );
+}
